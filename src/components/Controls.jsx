3d419ac19e2e89ead7ddbca114d290dcc9ec087c
Soe Myat Cho <soemyatchobgn@gmail.com>
2025-05-06 22:55:57 -0700

    player background change

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -28,18 +28,18 @@ const Controls = () => {
   return (
     <>
       {/* Progree Bar Section */}
-      <div className="relative h-1 bg-gray-400 rounded mb-4">
+      <div className="relative h-1 bg-gray-500 rounded mb-4">
         <div
-          className="absolute top-0 left-0 h-full bg-blue-400 shadow-sm shadow-blue-400 rounded"
+          className="absolute top-0 left-0 h-full bg-gray-100 shadow-sm shadow-gray-400 rounded"
           style={{ width: `${(currentTime / duration) * 100}%` }} // Update width based on current time
         >
-          <span className=" absolute right-0 top-[-100%] w-3 h-3 bg-blue-300 rounded-full"></span>
+          <span className=" absolute right-0 top-[-100%] w-3 h-3 bg-gray-600 rounded-full"></span>
         </div>
         <div className=" flex items-center justify-between pt-2">
           {/* current time */}
-          <span> {formatTime(currentTime)} </span>
+          <span className="text-gray-700"> {formatTime(currentTime)} </span>
           {/* max duration */}
-          <span> {formatTime(duration)} </span>
+          <span className="text-gray-700"> {formatTime(duration)} </span>
         </div>
 
         <div
@@ -53,14 +53,14 @@ const Controls = () => {
       <div className="flex justify-around items-center mt-10">
         {/* Previous Button */}
         <button onClick={prevSong} className=" ">
-          <FaStepBackward className=" hover:text-blue-600 duration-300" />
+          <FaStepBackward className=" hover:text-blue-600 text-gray-700 duration-300" />
         </button>
 
         {/* Play/Pause Button */}
         <div className="flex items-center justify-center">
           <button
             onClick={toggleSong}
-            className="relative p-4 bg-white rounded-full shadow-lg glow-button"
+            className="relative p-4 bg-white text-gray-700 rounded-full shadow-lg glow-button"
           >
             {isPlaying ? <FaPause className="" /> : <FaPlay className="" />}{" "}
             <div className="absolute inset-0 rounded-full animate-pulse bg-gradient-to-r from-blue-300 to-blue-700 opacity-30 glow-ring"></div>
@@ -69,7 +69,7 @@ const Controls = () => {
 
         {/* Next Button */}
         <button onClick={nextSong} className=" ">
-          <FaStepForward className=" hover:text-blue-600 duration-300" />
+          <FaStepForward className=" hover:text-blue-600 text-gray-700 duration-300" />
         </button>
       </div>
     </>
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -17,10 +17,13 @@ const MusicPlayer = () => {
   }, []);
 
   return (
-    <div className="px-10 pb-10 pt-4 bg-blue-200 rounded-xl  w-80 opacity-85 ">
+    //<div className="px-8 pb-8 pt-4 border border-white shadow-lg bg-gradient-to-br from-blue-200 via-gray-100 to-purple-300 rounded-2xl w-80 opacity-85 backdrop-blur-md">
+    // <div className="px-10 pb-10 pt-4 border border-gray-200 bg-gray-500 rounded-xl  w-80 opacity-85 ">
+    // <div className="px-10 pb-10 pt-4 border border-gray-200 bg-indigo-300 rounded-xl  w-80 opacity-90 shadow-sm shadow-white ">
+    <div className="px-10 pb-10 pt-4 border border-gray-200 bg-indigo-300/60 rounded-xl  w-80  shadow-sm shadow-white opacity-95">
       {/* Top Header Section */}
       <div className="flex items-center justify-between mb-4">
-        <h1 className="text-2xl text-white font-semibold">
+        <h1 className="text-2xl text-white  font-semibold">
           Taylor Swift on repeat
         </h1>
       </div>
